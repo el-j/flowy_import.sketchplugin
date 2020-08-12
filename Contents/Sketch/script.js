578face9895a3b71963bf880de111665c6cca504
falthaus <fabian.althaus@altran.com>
2020-08-12 16:12:57 +0200

    inital commit with working project import form flowybackend

--- Contents/Sketch/script.js
+++ Contents/Sketch/script.js
@@ -1,7 +1,8 @@
           var onRun = function(context) {
-          console.log('This is an example Sketch script.')
+
 
 const sketch = require('sketch')
+const UI = require('sketch/ui')
 const Style = sketch.Style
 const Artboard = sketch.Artboard
 const ShapePath = sketch.ShapePath
@@ -9,105 +10,204 @@ const Text = sketch.Text
 const Image = sketch.Image
 const Rectangle = sketch.Rectangle
 const api = 'http://localhost:9023/api'
-const projectsUrl = 'http://localhost:9023/projects'
+const projectsUrl = 'http://localhost:9023'
+Text.Alignment.left
 
+//we get all projects form the api
 const allData = networkRequest([`${api}/getProjects`])
 const allProjects = Object.keys(allData)
-
-
-let thisProject = networkRequest([`${api}/loadProject/:a-b`])
-console.log(thisProject.projectJson.nodes)
-let thisProjectNodes = Object.keys(thisProject.projectJson.nodes)
-let thisProjectLinks = Object.keys(thisProject.projectJson.links)
 let document = sketch.getSelectedDocument()
+// console.log(document);
 let page = document.selectedPage
-let allArtBoards = {}
 
 
-thisProjectNodes.map((node) => {
-  console.log(thisProject.projectJson.nodes[node].path)
+// the user has to choose the project that is loaded
+UI.getInputFromUser(
+  "Choose the flowy Project you want to render",
+  {
+   type: UI.INPUT_TYPE.selection,
+   possibleValues: allProjects,
+ },
+  (err, value) => {
+    if (err) {
+      UI.message('Flowy Canceled')
+      return
+    } else {
+      let idname = `flowy-identifier:${value}`
+        let isIdentifier = sketch.find(`[name="${idname}"]`)
+        if (isIdentifier.length > 0) {
+          // console.log("show me the result",isIdentifier,sketch.find(`Shape, [name="flowy-identifier:${value}"]`));
+          UI.message(`TODO: Update the Project: ${value}`)
+          updateProject(value)
+        }
+        else {
+          let identiferText = new ShapePath({
+            parent: page,
+            name: `flowy-identifier:${value}`,
+            frame: { x: 10000, y: 10000, width: 1, height: 1 },
+            style: { fills: ['#35E6C9']}
 
-
-  let width = thisProject.projectJson.nodes[node].size.width
-  let height = thisProject.projectJson.nodes[node].size.height
-  let x = thisProject.projectJson.nodes[node].position.x
-  let y = thisProject.projectJson.nodes[node].position.y
-  allArtBoards[node] = new Artboard({ parent: page,
-    frame: { x: x, y: y, width: width, height: height },
-    name: node
-  })
-  let myText = new Text({
-    parent: allArtBoards[node],
-    name: `nodeText${node}`,
-    text: `What a nice thing ${node}`
-  })
-
-  let imagePath = `${projectsUrl}${thisProject.projectJson.nodes[node].path}`
-  if (imagePath === 'http://localhost:9023/projects/no_image.png') {
-    imagePath = "http://localhost:9023/no_image.png"
+          })
+          loadProject(value)
+        }
+    }
   }
+)
 
-  let imageurl_nsurl = NSURL.alloc().initWithString(imagePath);
-  let nsimage = NSImage.alloc().initByReferencingURL(imageurl_nsurl);
-  console.log(nsimage);
-  let mySquare = new ShapePath({
-    name: node,
-    parent: allArtBoards[node],
-    frame: { x: 0, y: 0, width: width, height: height },
-    style: {
-    fills: [{
-      fill: 'Pattern',
-      pattern: {
-        patternType: Style.PatternFillType.Fill,
-        image: nsimage
-      }
-    }]
-  }
+
+function updateProject(project){
+  let thisProject = networkRequest([`${api}/loadProject/:${project}`])
+  let thisProjectNodes = Object.keys(thisProject.projectJson.nodes)
+  let thisProjectLinks = Object.keys(thisProject.projectJson.links)
+
+  let allArtBoards = {}
+  thisProjectNodes.map((node) => {
+
+    let currentArtboard = sketch.find(`[name="${thisProject.projectJson.nodes[node].name}"]`)
+    // console.log(currentArtboard);
+
   })
-//   var imageLayer = new Image({
-//     name:thisProject.projectJson.nodes[node].path,
-//         parent: allArtBoards[node],
-//   image: imageurl_nsurl,
-//   frame: new Rectangle(0, 0, 300, 200),
-// })
 
-  // thisProjectLinks.map((link) => {
-    // console.log(thisProject.projectJson.links[link])
-    // let width = thisProject.projectJson.links[link].size.width
-    // let height = thisProject.projectJson.links[link].size.height
-    // let x = thisProject.projectJson.links[link].position.x
-    // let y = thisProject.projectJson.links[link].position.y
-    // allArtBoards[node] = new Artboard({ parent: page,
-    //   frame: { x: x, y: y, width: width, height: height },
-    //   name: node
-    // })
-    // let myText = new Text({
-    //   parent: allArtBoards[node],
-    //   name: `nodeText${link}`,
-    //   text: `What a nice thing ${link}`
-    // })
-    // let mySquare = new ShapePath({
-    //     parent: allArtBoards[node],
-    //     frame: { x: 53, y: 213, width: 122, height: 122 },
-    //     style: { fills: ['#35E6C9']}
-    // })
+
+}
+
+function loadProject(project){
+
+  // we load the project from the backend
+  let thisProject = networkRequest([`${api}/loadProject/:${project}`])
+  let thisProjectNodes = Object.keys(thisProject.projectJson.nodes)
+  let thisProjectLinks = Object.keys(thisProject.projectJson.links)
+
+  let allArtBoards = {}
+
+  // we create all nodes as artboards
+  thisProjectNodes.map((node) => {
+    // console.log(thisProject.projectJson.nodes[node])
+    let width, height = ''
+    if (!thisProject.projectJson.nodes[node].size) {
+      width = thisProject.projectJson.nodes[node].position.width
+      height = thisProject.projectJson.nodes[node].position.height
+    }
+    else {
+      width = thisProject.projectJson.nodes[node].size.width
+      height = thisProject.projectJson.nodes[node].size.height
+    }
+    // console.log(thisProject.projectJson.nodes[node])
+    let x = thisProject.projectJson.nodes[node].position.x
+    let y = thisProject.projectJson.nodes[node].position.y
+    let nodeName = thisProject.projectJson.nodes[node].name
+    let artBoardName = thisProject.projectJson.nodes[node].name
+    if (artBoardName.includes('_')) {
+      artBoardName = artBoardName.replace(/_/g,' / ')
+    }
+
+    allArtBoards[node] = new Artboard({ parent: page,
+      frame: { x: x, y: y, width: width, height: height },
+      name: artBoardName
+    })
+
+    let nodeNameText = new Text({
+      parent: allArtBoards[node],
+      name: `${nodeName}`,
+      text: `${nodeName}`,
+      frame: { x: 20, y: perectage(85,height),width: width, height: 20},
+        style: {
+          textColor: '#111',
+          fontSize: 20,
+          lineHeight: null,
+          paragraphSpacing:perectage(120,20),
+          aligenment: 'left'
+        },
+    })
+    let nodeDescriptionText = new Text({
+      parent: allArtBoards[node],
+      name: `${thisProject.projectJson.nodes[node].text}`,
+      text: `${thisProject.projectJson.nodes[node].text}`,
+      frame: { x: 20, y: perectage(85,height)+perectage(120,20), width: width, height: 10 },
+        style: {
+          textColor: '#333',
+          lineHeight: null,
+          paragraphSpacing:perectage(120,10),
+          fontSize: 10,
+        },
+    })
+
+    nodeNameText.style.borders = [{enabled:false}]
+    nodeDescriptionText.style.borders = [{enabled:false}]
+
+    let imagePath = `${projectsUrl}/projects/${thisProject.projectJson.nodes[node].path}`
+    if (imagePath === 'http://localhost:9023/projects/no_image.png') {
+      imagePath = "http://localhost:9023/no_image.png"
+    }
+
+    let imageurl_nsurl = NSURL.alloc().initWithString(imagePath);
+    let nsimage = NSImage.alloc().initByReferencingURL(imageurl_nsurl);
+    // console.log(imagePath);
+    let mySquare = new ShapePath({
+      name: thisProject.projectJson.nodes[node].picture,
+      parent: allArtBoards[node],
+      frame: { x: 0, y: 0, width: width, height: perectage(80,height) },
+      style: {
+      fills: [{
+        fill: 'Pattern',
+        pattern: {
+          patternType: Style.PatternFillType.Fit,
+          image: nsimage
+        }
+      }]
+    }
+    })
+  //   var imageLayer = new Image({
+  //     name:thisProject.projectJson.nodes[node].path,
+  //         parent: allArtBoards[node],
+  //   image: imageurl_nsurl,
+  //   frame: new Rectangle(0, 0, 300, 200),
   // })
-})
 
-
-
-let selectedLayers = document.selectedLayers
-let selectedCount = selectedLayers.length
-
-if (selectedCount === 0) {
-  console.log('No layers are selected.')
-} else {
-  console.log('Selected layers:');
-  selectedLayers.forEach(function (layer, i) {
-    console.log((i + 1) + '. ' + layer.name)
+    // thisProjectLinks.map((link) => {
+      // console.log(thisProject.projectJson.links[link])
+      // let width = thisProject.projectJson.links[link].size.width
+      // let height = thisProject.projectJson.links[link].size.height
+      // let x = thisProject.projectJson.links[link].position.x
+      // let y = thisProject.projectJson.links[link].position.y
+      // allArtBoards[node] = new Artboard({ parent: page,
+      //   frame: { x: x, y: y, width: width, height: height },
+      //   name: node
+      // })
+      // let myText = new Text({
+      //   parent: allArtBoards[node],
+      //   name: `nodeText${link}`,
+      //   text: `What a nice thing ${link}`
+      // })
+      // let mySquare = new ShapePath({
+      //     parent: allArtBoards[node],
+      //     frame: { x: 53, y: 213, width: 122, height: 122 },
+      //     style: { fills: ['#35E6C9']}
+      // })
+    // })
   })
 }
 
+
+
+// let selectedLayers = document.selectedLayers
+// let selectedCount = selectedLayers.length
+//
+// if (selectedCount === 0) {
+//   console.log('No layers are selected.')
+// } else {
+//   console.log('Selected layers:');
+//   selectedLayers.forEach(function (layer, i) {
+//     console.log((i + 1) + '. ' + layer.name)
+//   })
+// }
+
+function perectage(input,max){
+  let temp = (max/100)*input
+  console.log(temp)
+  return temp
+}
+// helper function parsing the answer from the api
 function tryParseJSON (jsonString){
   try {
     let o = JSON.parse(jsonString);
@@ -125,6 +225,7 @@ function tryParseJSON (jsonString){
   return false;
 }
 
+// helper function doing the api requests
 function networkRequest(args) {
   const task = NSTask.alloc().init();
   task.setLaunchPath("/usr/bin/curl");
@@ -146,6 +247,7 @@ function networkRequest(args) {
   return parsed;
 }
 
+// helper function Object.keys() seems to not work inside sketch plugin.
 if (!Object.keys) {
   Object.keys = (function () {
     'use strict';
@@ -188,4 +290,3 @@ if (!Object.keys) {
 }
 
           };
-          
---
+++ readme.md
@@ -0,0 +1,5 @@
+# flowy sketch plugin
+
+
+this plugin is ment to load projects from flowy backend and
+create the charts inside sketch.
